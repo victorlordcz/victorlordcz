0db488117589a6ba9e85d934b6f74cd4178bbb3e
Victor Lord <107902972+victorlordcz@users.noreply.github.com>
2023-11-03 14:34:40 -0300

    Add files via upload

---
+++ My Profile.css
@@ -0,0 +1,24 @@
+### Hi! I'm Victor Lord 😁🤚
+
+- 🔭 I’m currently working at: Dell 
+- 🌱 I’m currently learning: Phyton‚ SQL‚ Java
+- 📫 How to reach me: victor_oliveira_lord@hotmail.com or on my LinkedIn
+
+
+### Connect With Me:
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/victor-lord-a97427239/)
+
+<div style="display: inline_block"><br>
+    <img align="center" alt="Victor-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+    <img align="center" alt="Victor-HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+    <img align="center" alt="Rafa-CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+    <img align="center" alt="Victor-Python" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg">
+    <img align="center" alt="Victor-MySQL" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg">
+    <img align="center" alt="Victor-Nodejs" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg">
+    <img align="center" alt="Victor-Java" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg">
+    <img align="center" alt="Victor-Oracle" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg">
+</div>
+
+##
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=victorlordcz&layout=compact)](https://github.com/victorlordcz/github-readme-stats)
